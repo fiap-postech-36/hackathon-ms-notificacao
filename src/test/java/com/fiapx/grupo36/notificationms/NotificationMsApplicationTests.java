db23f052dc8796ace667d6889ba4ad893f93d917
Samuel Molendolff Teixeira <contact@samuelmteixeira.dev>
2025-01-16 00:52:51 -0300

    build: add junit

--- src/test/java/com/fiapx/grupo36/notificationms/NotificationMsApplicationTests.java
+++ src/test/java/com/fiapx/grupo36/notificationms/NotificationMsApplicationTests.java
@@ -1,5 +1,6 @@
 package com.fiapx.grupo36.notificationms;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class NotificationMsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertTrue(true);
 	}
 
 }
